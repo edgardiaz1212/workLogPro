05a9051148050ce55cd49aace01a23a21dd753b4
Edgar Diaz <ed35376@gmail.com>
2023-11-14 19:26:42 -0400

    cambios navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,27 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoCantv from "../../img/cantv.jpg"
 
 export const Navbar = () => {
 	return (
 		<>
-		 <header id="header" class="fixed-top ">
-    <div class="container d-flex align-items-center">
+		 <header id="header" className="fixed-top ">
+    <div className="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html">DCCE</a></h1>
+      <h1 className="logo me-auto"><a href="index.html">DCCE</a></h1>
      
-      <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"/></a>
+      <a href="index.html" className="logo me-auto"><img src={logoCantv} alt="" className="img-fluid"/></a>
 
-      <nav id="navbar" class="navbar">
+      <nav id="navbar" className="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Servicios</a></li>
-          <li><a class="nav-link   scrollto" href="#portfolio">Portafolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Equipo</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a className="nav-link scrollto active" href="#hero">Inicio</a></li>
+          <li><a className="nav-link scrollto" href="#about">About</a></li>
+          <li><a className="nav-link scrollto" href="#services">Servicios</a></li>
+          <li><a className="nav-link   scrollto" href="#portfolio">Portafolio</a></li>
+          <li><a className="nav-link scrollto" href="#team">Equipo</a></li>
+          <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+              <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
                 <ul>
                   <li><a href="#">Deep Drop Down 1</a></li>
                   <li><a href="#">Deep Drop Down 2</a></li>
@@ -35,10 +36,10 @@ export const Navbar = () => {
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contacto</a></li>
-          <li><a class="getstarted scrollto" href="#about">Registro</a></li>
+          <li><a className="nav-link scrollto" href="#contact">Contacto</a></li>
+          <li><a className="getstarted scrollto" href="#about">Registro</a></li>
         </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
+        <i className="bi bi-list mobile-nav-toggle"></i>
       </nav>
 
     </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import baseImage from "../../img/hero-img.png";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -24,20 +24,53 @@ export const Home = () => {
     <div className="container">
       <div className="row">
         <div className="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
-          <h1>Better Solutions For Your Business</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <h1>Infraestructura DCCE</h1>
+          <h2>Varias soluciones un solo lugar</h2>
           <div className="d-flex justify-content-center justify-content-lg-start">
-            <a href="#about" className="btn-get-started scrollto">Get Started</a>
+            <a href="#about" className="btn-get-started scrollto">Comencemos</a>
             
           </div>
         </div>
         <div className="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-          <img src="../../img/hero-img.png" className="img-fluid animated" alt=""/>
+          <img src={baseImage} className="img-fluid animated" alt=""/>
         </div>
       </div>
     </div>
 
   </section>
-		</>
+  
+    <section id="about" className="about">
+      <div className="container" data-aos="fade-up">
+
+        <div className="section-title">
+          <h2>Infraestructura DCCE</h2>
+        </div>
+
+        <div className="row content">
+          <div className="col-lg-6">
+            <p>
+              Somos parte de la familia Data Center el Hatillo, garantes de que se cumplan las normativas y continuar la labor de la mejora continua
+            </p>
+            <ul>
+              <li><i className="ri-check-double-line"></i> Comunicacion constante con proveedores internos para asegurar la Infraestructura</li>
+              <li><i className="ri-check-double-line"></i> Verificacion de cumplimiento de acuerdos con los diversos proveedores</li>
+              <li><i className="ri-check-double-line"></i> Grupo integral para dar soluciones acertadas</li>
+            </ul>
+          </div>
+          <div className="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            
+          </div>
+        </div>
+
+      </div>
+    </section>
+    
+    
+    </>
 	);
 };
