364a03838f18dbf9e14eaf75f7cd1e26338b21c0
Edgar Diaz <ed35376@gmail.com>
2023-11-22 20:06:06 -0400

    tabla user, cambio register

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,15 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20),nullable=False)
+    surname=db.colum(db.String(20),nullable=False)
+    unit=db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    email_dch= db.Column(db.String(120), unique=True, nullable=False)
+    job_position=db.Column(db.String(120), nullable=False)
+    description=db.Column(db.String(120))
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +20,12 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "surname": self.surname,
+            "unit": self.unit,
             "email": self.email,
+            "email_dch": self.email_dch,
+            "job_position": self.job_position,
+            "description": self.description,
             # do not serialize the password, its a security breach
         }
--- src/front/js/pages/Register.jsx
+++ src/front/js/pages/Register.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 function Register() {
   return (
     <>
-      <div className="container ls">
-        <p>nombre , apellido, unidad, correo, correo alternativo, Puesto, descripcion</p>
+      <div className="container ls mt-4 ">
+       
         <div className="input-group mb-3">
           <span className="input-group-text" id="basic-addon1">Nombre </span>
           <input type="text" className="form-control" placeholder="Nombre" aria-label="name" aria-describedby="basic-addon1" />
@@ -21,10 +21,31 @@ function Register() {
           <span className="input-group-text" id="basic-addon4">Correo DCH </span>
           <input type="text" className="form-control" placeholder="@dch.cantv.com.ve" aria-label="email2" aria-describedby="basic-addon4" />
         </div>
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon5">Cargo </span>
+
+          <select
+            className="form-select"
+            aria-label="jobPosition"
+            id="jobPosition"
+            name="jobPosition"
+          // onChange={handleChange}
+          // value={newUser.contract}
+          >
+            <option value="">Seleccionar Cargo</option>
+            <option value="Consultor">Consultor</option>
+            <option value="Lider">Lider</option>
+            <option value="Gerente">Gerente</option>
+          </select>
+        </div>
+        <div class="input-group">
+          <span class="input-group-text">Descripcion</span>
+          <textarea class="form-control" aria-label="description"></textarea>
+        </div>
 
 
         <Link to="/">
-          <button className="btn btn-primary">Back home</button>
+          <button className="btn btn-primary mt-3">Back home</button>
         </Link>
       </div>
     </>
