31458c6a201d7397d713c01e266bfe3166c2c74c
Edgar Diaz <ed35376@gmail.com>
2023-11-24 20:38:25 -0400

    Update login user mutation to save token in local storage (generated by blackbox.ai )

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,7 +6,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			
+
 			registerUser: async (user) => {
 
 				const store = getStore()
@@ -26,34 +26,38 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			loginUser: async (email, password) => {
 				try {
-				  const response = await fetch(`${process.env.BACKEND_URL}/login`, {
-					method: "POST",
-					headers: {
-					  "Content-Type": "application/json",
-					},
-					body: JSON.stringify({ email, password }),
-				  });
-		
-				  const data = await response.json();
-		
-				  if (response.ok) {
-					setStore({ user: data.user });
-					// Puedes realizar otras acciones aquí, como redireccionar a otra página
-				  } else {
-					// Manejar errores, mostrar mensajes, etc.
-					console.error("Error logging in:", data.message);
-				  }
-		
-				  return response.status;
+					const response = await fetch(`${process.env.BACKEND_URL}/login`, {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ email, password }),
+					});
+
+					const data = await response.json();
+
+					if (response.ok) {
+						setStore({
+							user: data.user,
+							token: data.token
+						});
+						localStorage.setItem("token", data.token)
+						localStorage.setItem("user", data.user)
+					} else {
+						// Manejar errores, mostrar mensajes, etc.
+						console.error("Error logging in:", data.message);
+					}
+
+					return response.status;
 				} catch (error) {
-				  console.error("Error logging in:", error);
-				  return 500;
+					console.error("Error logging in:", error);
+					return 500;
 				}
-			  },
+			},
 			getUserData: async () => {
 				const store = getStore();
 				try {
-					const response = await fetch(`${process.env.BACKEND_URL}/user/by-alias`, {
+					const response = await fetch(`${process.env.BACKEND_URL}/user`, {
 						method: "GET",
 						headers: {
 							"Content-Type": "aplication/json",
@@ -73,7 +77,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 			},
 
-			
+
 		}
 	};
 };
