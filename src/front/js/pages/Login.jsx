e67062f0ede73a11c583dfbdf66432e316ac7a8a
Edgar Diaz <ed35376@gmail.com>
2023-12-03 20:47:03 -0400

    onkeydown login

--- src/front/js/pages/Login.jsx
+++ src/front/js/pages/Login.jsx
@@ -34,7 +34,7 @@ function Login() {
           navigate("/")
         }, 2000)
       } else {
-        toast.error(`Error de Inicio de sesión: ${data.message|| "Error desconocido"}`, {
+        toast.error(`Error de Inicio de sesión: ${data.message || "Error desconocido"}`, {
           position: "top-center",
           autoClose: 3000,
           hideProgressBar: false,
@@ -58,28 +58,35 @@ function Login() {
     });
   };
 
+  // Manejar la tecla "Enter" para enviar el formulario
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleLogin();
+    }
+  };
+
   return (
     <>
       <ToastContainer theme="dark" position="top-center" pauseOnFocusLoss={false} autoClose={3000} hideProgressBar />
       <div className="container m-5">
         <div className="row justify-content-md-center">
           <div className="col-md-auto">
-            <div className=" mb-3 ">
-              <label htmlFor="formControlInput1" className="form-label">Correo</label>
-              <input type="email" className="form-control" id="formControlInput1" placeholder="name@example.com" onChange={handleChange} name="email"/>
-            </div>
+            <form onKeyDown={handleKeyDown}>
+              <div className="mb-3">
+                <label htmlFor="formControlInput1" className="form-label">Correo</label>
+                <input type="email" className="form-control" id="formControlInput1" placeholder="name@example.com" onChange={handleChange} name="email"/>
+              </div>
 
-            <label htmlFor="inputPassword" className="form-label">Password</label>
-            <input type="password" id="inputPassword" className="form-control" aria-describedby="passwordHelpBlock" onChange={handleChange} name="password" />
-            {/* <div id="passwordHelpBlock" class="form-text">
-  Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-</div> */}
-            <button type="button" className="btn btn-primary mt-3"  onClick={() => handleLogin()}>Entrar</button>
+              <label htmlFor="inputPassword" className="form-label">Password</label>
+              <input type="password" id="inputPassword" className="form-control" aria-describedby="passwordHelpBlock" onChange={handleChange} name="password" />
 
+              <button type="button" className="btn btn-primary mt-3" onClick={() => handleLogin()}>Entrar</button>
+            </form>
           </div>
         </div>
       </div>
     </>
   )
 }
-export default Login
+
+export default Login;
