8a40a9cab73fd5dd4e92dc6320473ed08d0167e5
Edgar Diaz <127860772+edgardiaz1212@users.noreply.github.com>
2023-12-08 17:27:32 +0000

    MaintenanceEvidence en admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Activity, Documents
+from .models import db, User, Activity, Documents, MaintenanceEvidence
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -14,6 +14,7 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Activity, db.session))
     admin.add_view(ModelView(Documents, db.session))
+    admin.add_view(ModelView(MaintenanceEvidence, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
