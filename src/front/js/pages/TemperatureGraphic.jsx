556dc27454f8f97846b236e1c2c0e26d502c96cd
Edgar Diaz <127860772+edgardiaz1212@users.noreply.github.com>
2023-12-18 18:53:49 +0000

    graficas temp

--- src/front/js/pages/TemperatureGraphic.jsx
+++ src/front/js/pages/TemperatureGraphic.jsx
@@ -29,7 +29,50 @@ function TemperatureGraphic() {
     try {
       // Obtener datos por trimestre para el año seleccionado
       const data = await getTemperatureByQuarter(selectedYear);
-      setQuarterlyData(data);
+
+      // Procesar datos para obtener promedio por día y air_unit
+      const processedData = {};
+      Object.keys(data).forEach((quarter) => {
+        processedData[quarter] = {};
+
+        data[quarter].forEach((entry) => {
+          const dateKey = `${entry.year}-${entry.month}-${entry.day}`;
+          const airUnit = entry.air_unit;
+
+          // Crear un objeto para el día si no existe
+          if (!processedData[quarter][dateKey]) {
+            processedData[quarter][dateKey] = {};
+          }
+
+          // Crear un array para el air_unit si no existe
+          if (!processedData[quarter][dateKey][airUnit]) {
+            processedData[quarter][dateKey][airUnit] = [];
+          }
+
+          // Agregar la temperatura al array
+          processedData[quarter][dateKey][airUnit].push(entry.temperature);
+        });
+      });
+
+      // Calcular el promedio por día y air_unit
+      const averagedData = {};
+      Object.keys(processedData).forEach((quarter) => {
+        averagedData[quarter] = [];
+
+        Object.keys(processedData[quarter]).forEach((dateKey) => {
+          const dateAverages = Object.keys(processedData[quarter][dateKey]).map((airUnit) => ({
+            date: new Date(dateKey),  // Convertir la cadena de fecha a objeto de fecha
+            airUnit,
+            averageTemperature:
+              processedData[quarter][dateKey][airUnit].reduce((sum, temp) => sum + temp, 0) /
+              processedData[quarter][dateKey][airUnit].length,
+          }));
+
+          averagedData[quarter] = averagedData[quarter].concat(dateAverages);
+        });
+      });
+
+      setQuarterlyData(averagedData);
     } catch (error) {
       console.error("Error al obtener datos por trimestre", error);
     }
@@ -53,36 +96,32 @@ function TemperatureGraphic() {
           <h3>{`Trimestre ${index + 1}`}</h3>
           <Line
             data={{
-              labels: quarterlyData[quarter].map((entry) => entry.measurement_time),
-              datasets: [
-                {
-                  label: 'Temperatura',
-                  data: quarterlyData[quarter].map((entry) => entry.temperature),
-                  borderColor: 'rgba(75,192,192,1)',
+              labels: quarterlyData[quarter].map((entry) => entry.date.toISOString().slice(0, 10)),  // Formatear la fecha como cadena
+              datasets: quarterlyData[quarter].length > 0
+                ? Object.keys(quarterlyData[quarter][0]).map((airUnit, airUnitIndex) => ({
+                  label: `Air Unit ${airUnitIndex + 1}`,
+                  data: quarterlyData[quarter]
+                    .filter((entry) => entry.airUnit === airUnit)
+                    .map((entry) => entry.averageTemperature),
+                  borderColor: `rgba(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255},1)`,
                   borderWidth: 2,
                   fill: false,
-                },
-              ],
+                }))
+                : [],
             }}
             options={{
               scales: {
                 x: {
-                  type: 'linear',
-                  position: 'bottom',
-                  beginAtZero: true,
-                  title: {
-                    display: true,
-                    text: 'Meses',
+                  type: 'time',  // Cambiado a 'time' para manejar fechas
+                  time: {
+                    unit: 'month',  // Ajusta la unidad de tiempo según tus necesidades
                   },
+                  position: 'bottom',
                 },
                 y: {
                   type: 'linear',
                   position: 'left',
                   beginAtZero: true,
-                  title: {
-                    display: true,
-                    text: 'Temperatura',
-                  },
                 },
               },
             }}
